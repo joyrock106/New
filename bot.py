23cdaeccfdf17c4f23c5ba846e162dbcfcc14f50
Joy <joymajumdar590@gmail.com>
2025-07-17 16:43:15 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import time
 import re
 import json
 
-BOT_TOKEN = "7994446557:AAFA2u-ZGQ32lEAES6eRrhconrFOLo_nE18"
+BOT_TOKEN = "7659176544:AAEhXMiwIdeYkL_m-yGN4GMl8uuITe81gL0"
 bot = telebot.TeleBot(BOT_TOKEN)
 
 MAX_SIZE = 2 * 1024 * 1024 * 1024  # 2GB
@@ -17,7 +17,7 @@ if os.path.exists(DATA_FILE):
     with open(DATA_FILE, 'r') as f:
         access_data = json.load(f)
 else:
-    access_data = {"admin_id": None, "approved_users": [], "pending_requests": []}
+    access_data = {"8078418903": None, "approved_users": [], "pending_requests": []}
 
 def save_access_data():
     with open(DATA_FILE, 'w') as f:
